ef2c5bfed76d302fb51a4a88b7726517324c5048
ordinary-developer <merely.ordinary.developer@gmail.com>
2016-10-22 21:15:12 +0300

    change the first qt program
    change the first qt-program
    (make the cpp file more c++14-standard-complient
    and add c++14 support for qmake)

--- code/chapter_1-QT_CLASS_HIERARСHY_REVIEW/01_the_first_qt_program/hello.cpp
+++
@@ -1,8 +0,0 @@
-#include <QtWidgets>
-
-int main(int argc, char** argv) {
-    QApplication application(argc, argv);
-    QLabel label("Hello, World!");
-    label.show();
-    return application.exec();
-}
